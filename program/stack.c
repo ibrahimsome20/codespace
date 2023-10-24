6dd85f355b9e3738394f4f76bc153e1905e97805
ibrahimsome20 <141369951+ibrahimsome20@users.noreply.github.com>
2023-10-24 22:04:22 +0000

    Tue, Oct 24, 2023, 10:04 PM +00:00

--- program/stack.c
+++ program/stack.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 void draw(int n){
-    if(n==0){return;}
-  for(int i=0;i<n;i++){
+ int x=n-(n-1);
+    if(x==n){return;}
+  for(int i=0;i<x;i++){
     printf("#");
   }
 printf("\n");
-  draw(n-1);
+  draw(n+1);
 }
 
 int main (void){
