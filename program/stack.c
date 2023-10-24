70e1c58dff150552fe96e3479727b24a1e3cf510
ibrahimsome20 <141369951+ibrahimsome20@users.noreply.github.com>
2023-10-24 20:37:45 +0000

    Tue, Oct 24, 2023, 8:37 PM +00:00

--- program/stack.c
+++ program/stack.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 
-int plus(int x,int y){
-    int result=x+y;
-    return result;
-}
+
 int main (void){
-    for(int i=0;i<5;i++){
-       for(int j=0;j<5;j++){
-        printf("#");
-       }
-       printf("\n");
+    printf("ahmed");
     }
-printf("\n");
-}
+
