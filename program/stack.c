d338664f7547d85ec8b259ec95316b7859819875
ibrahimsome20 <141369951+ibrahimsome20@users.noreply.github.com>
2023-10-24 19:24:30 +0000

    Tue, Oct 24, 2023, 7:24 PM +00:00

--- program/stack.c
+++ program/stack.c
@@ -6,7 +6,10 @@ int plus(int x,int y){
 }
 int main (void){
     for(int i=0;i<5;i++){
-       printf("#");
+       for(int j=0;j<5;j++){
+        printf("#");
+       }
+       printf("\n");
     }
 printf("\n");
 }
