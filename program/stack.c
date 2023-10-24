4126fd40a0452d3d291e2bb2a6c1600486c1d659
ibrahimsome20 <141369951+ibrahimsome20@users.noreply.github.com>
2023-10-24 19:17:49 +0000

    Tue, Oct 24, 2023, 7:17 PM +00:00

--- program/stack.c
+++ program/stack.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 int plus(int x,int y){
     int result=x+y;
     return result;
 }
 int main (void){
-    printf("hello world\n");
-    int x= plus(50,20);
-    printf("your result is %i\n"x);
+    for(int i=0,i<5,i++){
+       printf("#");
+    }
+    printf("\n");
 }
