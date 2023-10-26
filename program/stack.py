a1d71d4465907a2ea66c17f062e6d80d8d8905fc
ibrahimsome20 <141369951+ibrahimsome20@users.noreply.github.com>
2023-10-26 11:15:49 +0000

    Thu, Oct 26, 2023, 11:15 AM +00:00

--- program/stack.py
+++ program/stack.py
@@ -1,9 +1,10 @@
 while True:
     num1=input("enter first number")
     num2=input("enter your second number")
-     if type(num1)== <class 'str'> or type(num2) == <class 'str'>:
+    if type(num1)== str or type(num2) == str:
           print("you must write number")
-      if num1 >=0 and num2 >=0:
+          
+    elif num1 >=0 and num2 >=0:
             break
 
 def sum_number(num1,num2):
