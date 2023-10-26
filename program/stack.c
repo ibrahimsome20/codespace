f626930e128d48cd7ad9d4f8ffc136d22999ace1
ibrahimsome20 <141369951+ibrahimsome20@users.noreply.github.com>
2023-10-26 11:49:03 +0000

    Thu, Oct 26, 2023, 11:49 AM +00:00

--- program/stack.c
+++ program/stack.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
 
-float sum(float x,float y){
+void sum(int x,int  y){
     float result=x+y;
     printf("%f\n",result);
 }
-void int main(){
-float num1=get_float("enter your first number: ");
-float num2=get_float("enter your secound number: ");
+int main(void){
+float num1=get_int("enter your first number: ");
+float num2=get_int("enter your secound number: ");
 sum(num1,num2);
 
 }
