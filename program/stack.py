591bff7d9a72dffe329f6e00c475c66e1a337960
ibrahimsome20 <141369951+ibrahimsome20@users.noreply.github.com>
2023-10-26 10:58:50 +0000

    Thu, Oct 26, 2023, 10:58 AM +00:00

--- program/stack.py
+++ program/stack.py
@@ -1,13 +1,10 @@
 while True:
     num1=(input("enter first number"))
     num2=(input("enter your second number"))
-    if num1 >=0 && num2 >=0:
+    if num1 >=0 and num2 >=0:
         break
 
-
-
-
-def sum_number(int(num1),int(num2)):
+def sum_number((num1),int(num2)):
     result=num1+num2
     return result
 
