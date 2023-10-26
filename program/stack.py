c5423ca6053d865c883f7e7395972be9754e1be0
ibrahimsome20 <141369951+ibrahimsome20@users.noreply.github.com>
2023-10-26 11:05:17 +0000

    Thu, Oct 26, 2023, 11:05 AM +00:00

--- program/stack.py
+++ program/stack.py
@@ -1,8 +1,9 @@
 while True:
     num1=input("enter first number")
     num2=input("enter your second number")
-    print(type(str())
-    # if type(num1)==str() or type(num2)==str():
+    print(type(num1))
+
+    # if type(num1)==<> or type(num2)==str():
 
     #     if num1 >=0 and num2 >=0:
     #         break
