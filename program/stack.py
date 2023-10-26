5ae8fb6fe23943cff0bfe7f76a890ed573f2fbd5
ibrahimsome20 <141369951+ibrahimsome20@users.noreply.github.com>
2023-10-26 11:12:44 +0000

    Thu, Oct 26, 2023, 11:12 AM +00:00

--- program/stack.py
+++ program/stack.py
@@ -1,7 +1,7 @@
 while True:
     num1=input("enter first number")
     num2=input("enter your second number")
-     if type(num1)==<class 'str'> or type(num2) == <class 'str'>:
+     if type(num1)== <class 'str'> or type(num2) == <class 'str'>:
           print("you must write number")
       if num1 >=0 and num2 >=0:
             break
