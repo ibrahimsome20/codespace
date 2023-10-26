af048a43a00fab2b252bc1083c3cc82732bb5c6a
ibrahimsome20 <141369951+ibrahimsome20@users.noreply.github.com>
2023-10-26 11:23:35 +0000

    Thu, Oct 26, 2023, 11:23 AM +00:00

--- program/stack.py
+++ program/stack.py
@@ -1,9 +1,9 @@
 while True:
-    num1=input("enter first number")
-    num2=input("enter your second number")
+    num1=int(input("enter first number"))
+    num2=int(input("enter your second number"))
     if type(num1)== str or type(num2) == str:
           print("you must write number")
-          
+
           exit()
     elif num1 >=0 and num2 >=0:
             continue
